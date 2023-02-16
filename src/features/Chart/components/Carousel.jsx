918d111b0317fd3ed91448db7a5b696466580485
Huy <lenambaohuy107@gmail.com>
2023-02-16 11:01:41 +0700

    work load

--- src/features/Chart/components/Carousel.jsx
+++ src/features/Chart/components/Carousel.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
+import {
+  DownCircleOutlined,
+  UpCircleOutlined,
+} from "@ant-design/icons/lib/icons";
+import Marquee from "react-fast-marquee";
 
 const Carousel = () => {
+  const dataCarousel = useSelector((state) => state.chart.dataCarousel);
+  console.log(dataCarousel);
   return (
-    <div>
-      
+    <div className=" bg-slate-700 ">
+      <Marquee  speed={100} pauseOnHover="true" gradient="false" >
+        <div className="flex">
+          {dataCarousel.recordset?.map((item, index)=> {
+            return (
+              <div>
+                <span>{item.ticker}: {item.diemso}</span>
+              </div>
+            )
+          })}
+        </div>
+      </Marquee>
     </div>
-  )
-}
+  );
+};
 
-export default Carousel
+export default Carousel;
