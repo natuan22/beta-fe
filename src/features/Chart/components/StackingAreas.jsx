375c3dcd1d3f62debaf16e92cc30f111cb40d9fc
Huy <lenambaohuy107@gmail.com>
2023-03-07 08:40:33 +0700

    stacking chart

--- src/features/Chart/components/StackingAreas.jsx
+++ src/features/Chart/components/StackingAreas.jsx
@@ -1,71 +1,34 @@
-import React, { useRef } from "react";
+import React from "react";
 import Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
 import { useSelector } from "react-redux";
 import Loading from "../utils/Loading";
 import moment from "moment";
-import '../utils/Stacking.css'
 
 const PercentageAreaChart = () => {
-  const chartRef = useRef(null)
   const dataStackingChart = useSelector(
     (state) => state.chart.dataStackingArea
   );
   console.log(dataStackingChart.data);
-  if (!dataStackingChart.data || !dataStackingChart.data.length)
+  if ( !dataStackingChart.data || !dataStackingChart.data.length)
     return <Loading />;
 
   // Tiếp tục xử lý dữ liệu của bạn ở đây
   const timeLine = dataStackingChart.data?.map((item) =>
     moment(item.time, "HH:mm:ss").format("HH:mm")
   );
-  const totalValue = dataStackingChart.data?.map(item => (item.advance + item.noChange + item.decline))
-  const dataAdvance = dataStackingChart.data?.map((item) => (item.advance));
-  const dataDecline = dataStackingChart.data?.map((item) => (item.decline));
-  const dataNoChange = dataStackingChart.data?.map((item) => (item.noChange));
+  const totalValue = dataStackingChart.data?.map(
+    (item) => item.advance + item.noChange + item.decline
+  );
+  const dataAdvance = dataStackingChart.data?.map((item) => item.advance);
+  const dataDecline = dataStackingChart.data?.map((item) => item.decline);
+  const dataNoChange = dataStackingChart.data?.map((item) => item.noChange);
 
   const options = {
     chart: {
       type: "area",
       zoomType: "x",
       backgroundColor: "transparent",
-      events: {
-        mouseMove: function (e) {
-          const chart = this;
-          const legendData = [];
-          chart.series.forEach(function (series) {
-            const point = series.searchPoint(e, true);
-            if (point) {
-              const { x, y } = point;
-              const xAxis = chart.xAxis[0];
-              const xValue = xAxis.categories[x];
-              const yValue = y.toFixed(2);
-              legendData.push({
-                name: series.name,
-                color: series.color,
-                value: `${xValue}: ${yValue}`,
-              });
-            }
-          });
-    
-          chart.legend.update({
-            labelFormatter: function () {
-              const { name, color } = this;
-              const legendItem = legendData.find(item => item.name === name);
-              return `<span style="color: ${color}; cursor: pointer;">${name}: ${legendItem ? legendItem.value : ''}</span>`;
-            },
-          });
-        },
-        mouseOut: function () {
-          const chart = this;
-          chart.legend.update({
-            labelFormatter: function () {
-              return this.name;
-            },
-          });
-        },
-      },
-      
     },
     title: {
       text: "Độ rộng thị trường",
@@ -77,13 +40,13 @@ const PercentageAreaChart = () => {
       categories: timeLine,
       tickmarkPlacement: "on",
       title: {
-        enabled: false,
+        enabled: true,
       },
       labels: {
         style: {
-          color:'#fff'
-        }
-      }
+          color: "#fff",
+        },
+      },
     },
     yAxis: {
       title: {
@@ -104,19 +67,17 @@ const PercentageAreaChart = () => {
     legend: {
       itemStyle: {
         color: "#fff",
-      },  
+      },
+    
+     
     },
     tooltip: {
-      pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.change}%)<br/>',
-      split: false,
       shared: true,
-      outside: true,
-      positioner: function() {
-        return {
-          x: this.chart.Left,
-          y: 390
-        };
-      },
+    useHTML: true,
+    valueSuffix: " ",
+    pointFormatter: function() {
+      return '<span style="color:' + this.series.color + '">' + this.series.name + ': <b>' + this.y + '</b></span><br/>';
+    },
     },
     plotOptions: {
       area: {
@@ -126,6 +87,15 @@ const PercentageAreaChart = () => {
         marker: {
           lineWidth: 1,
           lineColor: "#ffffff",
+        },
+        events: {
+          legendItemClick: function () {
+            const chart = this.chart;
+            const seriesIndex = this.index;
+            const series = chart.series[seriesIndex];
+            series.visible ? series.hide() : series.show();
+            chart.redraw();
+          },
         },
       },
     },
@@ -162,5 +132,3 @@ const PercentageAreaChart = () => {
 };
 
 export default PercentageAreaChart;
-
-
