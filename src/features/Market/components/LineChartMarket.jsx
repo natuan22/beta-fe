3b332883f2aa911d01a87408c324704ee907bf75
Huy <lenambaohuy107@gmail.com>
2023-04-07 11:56:51 +0700

    a

--- src/features/Market/components/LineChartMarket.jsx
+++ src/features/Market/components/LineChartMarket.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
-import moment from "moment-timezone";
+import moment from "moment";
 import Loading from "../../Chart/utils/Loading";
 
 const LineChartMarket = (props) => {
