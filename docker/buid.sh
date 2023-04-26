f84e24f5b794ea622f52087d56110af42a48522e
Nguyen Thanh Minh <ngthminh.dev@gmail.com>
2023-04-26 09:36:57 +0700

    Fix: Fix docker build

--- docker/buid.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/zsh
-
-VERSION="0.0.35"
-
-docker build -t ngthminhdev/stock-client-demo:${VERSION} --build-arg REACT_APP_BASE_URL=http://192.168.13.20:3002 --build-arg REACT_APP_SOCKET_URL=http://192.168.13.20/socket .
-docker push ngthminhdev/stock-client-demo:${VERSION}
