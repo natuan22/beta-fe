d6c7d9a64563e0c189f515206cf093423bbc15a6
EnAyTi <anhtuan222001@gmail.com>
2023-05-19 10:32:47 +0700

    fix bug thunk

--- src/features/Market/marketSlice.js
+++ src/features/Market/marketSlice.js
@@ -15,8 +15,8 @@ const initialState = {
   dataRSI: {},
   dataTopNetBuyIndustry: {},
 
-  
-  dataTotalMarket:{}
+
+  dataTotalMarket: {},
 
   dataCashFlowInvestor: {},
   dataCashFlowRatio: {},
@@ -67,10 +67,9 @@ const reducer = (state = initialState, { type, payload }) => {
     if (type === marketType.FETCH_DATA_CASHFLOW_INVESTOR) {
       draft.dataCashFlowInvestor = payload
     }
-
-    if(type === marketType.FETCH_DATA_TOTAL_MARKET) {
+    if (type === marketType.FETCH_DATA_TOTAL_MARKET) {
       draft.dataTotalMarket = payload
-
+    }
     if (type === marketType.FETCH_DATA_CASHFLOW_RATIO) {
       draft.dataCashFlowRatio = payload
 
