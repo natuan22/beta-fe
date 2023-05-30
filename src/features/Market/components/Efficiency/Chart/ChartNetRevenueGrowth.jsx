cfc1dbdd1913fe0546942c6a1e894fdac33d313e
baohuydev <lenambaohuy1998@gmail.com>
2023-05-30 10:07:20 +0700

    colums chart efficency

--- src/features/Market/components/Efficiency/Chart/ChartNetRevenueGrowth.jsx
+++ src/features/Market/components/Efficiency/Chart/ChartNetRevenueGrowth.jsx
@@ -9,7 +9,7 @@ const ChartNetRevenueGrowth = (props) => {
     const { exchange, industryQuery, order, timeFrame } = props
     const { dataChartNetRevenueGrowth } = useSelector(state => state.market)
     const [data, setData] = useState()
-    const [timeLine, setTimeLine] = useState()
+    const [category, setCategory] = useState()
     const [colorText, setColorText] = useState(localStorage.getItem('color'));
     const color = useSelector((state) => state.color.colorText);
 
@@ -17,12 +17,108 @@ const ChartNetRevenueGrowth = (props) => {
         dispatch(fetchDataChartNetRevenueGrowth(exchange, industryQuery, timeFrame, order))
         setColorText(color);
     }, [props, color])
+    useEffect(() => {
+        if (dataChartNetRevenueGrowth?.length > 0) {
+            const transformedData = dataChartNetRevenueGrowth?.map(item => {
+                const year = item.date.slice(0, 4);
+                const quarter = item.date.slice(4);
+                const transformedDate = `Q${quarter} ${year}`;
+                return { ...item, date: transformedDate };
+            });
+            const uniqueIndustry = [...new Set(transformedData?.map(item => item.industry))];
+            const mappedData = [];
 
-    // console.log('dataChartNetRevenueGrowth', dataChartNetRevenueGrowth);
+            transformedData.forEach(item => {
+                const existingItem = mappedData.find(mappedItem => mappedItem.name === item.date);
+                if (existingItem) {
+                    existingItem.data.push(+(item.perChange).toFixed(2));
+                } else {
+                    mappedData.push({
+                        name: item.date,
+                        data: [+(item.perChange).toFixed(2)]
+                    });
+                }
+            });
+            setCategory(uniqueIndustry)
+            setData(mappedData)
+        }
+    }, [dataChartNetRevenueGrowth])
+    // config chart
+    const options = {
+        chart: {
+            backgroundColor: "transparent", // màu nền của biểu đồ
+            type: 'column'
+        },
+        accessibility: {
+            enabled: false
+        },
+        credits: false,
+        title: {
+            text: "",
+            style: {
+                color: 'white'
+            }
+        },
+        xAxis: {
+            categories: category,
+            labels: {
+                style: {
+                    color: localStorage.getItem('color') // màu cho các nhãn trục x
+                }
+            },
+            title: {
+                style: {
+                    color: localStorage.getItem('color') // màu cho tiêu đề trục x
+                }
+            }
+        },
+        yAxis: [
+            {
+                title: {
+                    text: "",
+                    style: {
+                        color: localStorage.getItem('color'),
+                    },
+                },
+                labels: {
+                    style: {
+                        color: localStorage.getItem('color') // màu cho các nhãn trục y
+                    },
+                    formatter: function () {
+                        return this.value + "%";
+                    },
+                }
+            },
+            {
+                title: {
+                    text: "",
+                    style: {
+                        color: localStorage.getItem('color'),
+                    },
+                },
+                labels: {
+                    style: {
+                        color: localStorage.getItem('color') // màu cho các nhãn trục y
+                    }
+                },
+                opposite: true,
+            },
 
+        ],
+        legend: {
+            align: 'center',
+            itemStyle: {
+                color: localStorage.getItem('color')
+            }
+        },
+
+        series: data,
+    };
     return (
         <>
-            <div>ChartNetRevenueGrowth</div>
+            <div>
+                <HighchartsReact highcharts={Highcharts} options={options} containerProps={{ style: { height: '100%', width: '100%' } }} />
+            </div>
         </>
     )
 }
