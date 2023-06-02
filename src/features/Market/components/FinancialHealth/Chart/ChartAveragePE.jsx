fb54fb77c81f44b8ae86d85a16b85725d181be03
baohuydev <lenambaohuy1998@gmail.com>
2023-06-02 09:10:55 +0700

    merge code

--- src/features/Market/components/FinancialHealth/Chart/ChartAveragePE.jsx
+++ src/features/Market/components/FinancialHealth/Chart/ChartAveragePE.jsx
@@ -82,111 +82,16 @@ const ChartAveragePE = (props) => {
 
         fetchDataAPI();
     }, [])
-    console.log(data)
-    return (
-        <div>
-            <div>ChartAveragePE</div>
-        </div>
-=======
-    const dispatch = useDispatch()
-    const { dataChartAveragePE } = useSelector((state) => state.market);
-    const [data, setData] = useState([]);
-    const [timeLine, setTimeLine] = useState()
-    const [colorText, setColorText] = useState(localStorage.getItem('color'));
-    const color = useSelector((state) => state.color.colorText);
 
-    useEffect(() => {
-        dispatch(fetchDataChartAveragePE(props.exchange, props.industryQuery, props.timeFrame, props.order));
-        setColorText(color);
-    }, [dispatch, props, color]);
 
-    useEffect(() => {
-        if (dataChartAveragePE?.length > 0) {
-            const mappedData = [];
-            const uniqueDates = [...new Set(dataChartAveragePE?.map(item => moment(item.date).format('DD/MM/YYYY')))];
-            setTimeLine(uniqueDates)
-            dataChartAveragePE.forEach(item => {
-                const existingItem = mappedData.find(mappedItem => mappedItem.name === item.industry);
-                if (existingItem) {
-                    existingItem.data.push(+(item.value).toFixed(2));
-                } else {
-                    mappedData.push({
-                        name: item.industry,
-                        color: item.color,
-                        data: [item.value]
-                    });
-                }
-            });
-            setData(mappedData)
-        }
-    }, [dataChartAveragePE])
-
-    const options = {
-        accessibility: {
-            enabled: false,
-        },
-        credits: false,
-        chart: {
-            type: "spline",
-            backgroundColor: "transparent",
-        },
-        title: {
-            text: null,
-        },
-        legend: {
-            enabled: false,
-            itemStyle: {
-                color: localStorage.getItem('color'),
-                fontWeight: 'bold'
-            }
-        },
-        xAxis: [{
-            categories: timeLine,
-            title: {
-                text: null,
-                style: {
-                    color: localStorage.getItem('color'),
-                },
-            },
-            labels: {
-                style: {
-                    color: localStorage.getItem('color'),
-                    fontSize: '9px',
-                },
-            },
-        }],
-        yAxis: {
-            title: {
-                text: null
-            },
-            labels: {
-                style: {
-                    color: localStorage.getItem('color'),
-                },
-            },
-        },
-        plotOptions: {
-            series: {
-                marker: {
-                    radius: 2, // Giá trị bán kính marker
-                },
-            }
-        },
-        series: data
-    }
     return (
         <>
-            {dataChartAveragePE.length ? (
-                <div id="chart-container">
-                    <div className="h-[450px] mt-3">
-                        <HighchartsReact highcharts={Highcharts} options={options} containerProps={{ style: { height: '100%', width: '100%' } }} />
-                    </div>
+            <div id="chart-container">
+                <div className="h-[450px] mt-3">
+                    <HighchartsReact highcharts={Highcharts} containerProps={{ style: { height: '100%', width: '100%' } }} />
                 </div>
-            ) : (
-                <div id="chart-container">
-                    <div className="mt-14 mb-24"><Loading /></div>
-                </div>
-            )}
+            </div>
+
         </>
 
     )
