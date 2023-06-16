aa1c4c83eb0e2928f95bfb3e5ce460ca187f31a6
baohuydev <lenambaohuy1998@gmail.com>
2023-06-16 15:31:49 +0700

    feat: macro

--- src/features/Macro/Tabs/DomesticMacro.jsx
+++ src/features/Macro/Tabs/DomesticMacro.jsx
@@ -1,30 +1,32 @@
-import React from 'react'
+import React from 'react';
 import { Tabs } from 'antd';
 import { hashTb } from '../utils/antComponentChild';
+
 const DomesticMacro = () => {
     const tabItems = Object.keys(hashTb).map((key, index) => {
         const Component = hashTb[key];
         return {
-            label: (<span className='text-white'>{key}</span>),
+            label: (
+                <span className='text-white ' >
+                    {key}
+                </span>
+            ),
             key: String(index),
             children: <Component />,
         };
     });
+
     return (
         <div>
-            <div>
-                <Tabs
-                    defaultActiveKey="1"
-                    tabPosition='top'
-                    style={{
-                        height: 220,
-                        color: 'yellow'
-                    }}
-                    items={tabItems}
-                />
-            </div>
+            <Tabs
+                defaultActiveKey="0"
+                tabPosition='top'
+                className=' text-yellow-300 '
+                items={tabItems}
+                tabBarStyle={{ display: 'grid', placeItems: 'center' }}
+            />
         </div>
-    )
-}
+    );
+};
 
-export default DomesticMacro
+export default DomesticMacro;
