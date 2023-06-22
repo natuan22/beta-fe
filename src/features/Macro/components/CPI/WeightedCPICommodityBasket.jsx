3d91de3950d55e6cc44e23902e4de89c7f0a5a12
EnAyTi <anhtuan222001@gmail.com>
2023-06-22 14:25:29 +0700

    build layout & build chart site vimo:cpi

---
+++ src/features/Macro/components/CPI/WeightedCPICommodityBasket.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const WeightedCPICommodityBasket = () => {
+    return (
+        <div className='dark:text-white text-black h-[300px]'>WeightedCPICommodityBasket</div>
+    )
+}
+
+export default WeightedCPICommodityBasket
