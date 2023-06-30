d605b565060f0f4b89f89cb528de8bc8bcdcc721
baohuydev <lenambaohuy1998@gmail.com>
2023-06-30 14:15:13 +0700

    feat: trung tam tin tuc

--- src/features/Chart/utils/socket.js
+++ src/features/Chart/utils/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client'
 const socketUrl = process.env.REACT_APP_SOCKET_URL;
-const socket = io(socketUrl,);
+const socket = io(socketUrl,{transports: ['websocket']});
 
 export default socket;
 
