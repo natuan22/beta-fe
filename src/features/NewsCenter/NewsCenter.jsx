4f937b9fded756d95f51209bae2dc4eb6e9a9a9f
baohuydev <lenambaohuy1998@gmail.com>
2023-06-30 14:22:18 +0700

    merge code: 30/06/2023

--- src/features/NewsCenter/NewsCenter.jsx
+++ src/features/NewsCenter/NewsCenter.jsx
@@ -3,11 +3,12 @@ import { Outlet } from 'react-router-dom';
 import LayOut from '../../HOCs/Layout'
 import NewsCenterNavTab from './utils/component/NewsCenterNavTab'
 import InternationalIndex from '../Chart/components/InternationalIndex';
+import Error404 from '../Navigation/Error404';
 
 const NewsCenter = () => {
     return (
         <LayOut>
-            <div>
+            {/* <div>
                 <InternationalIndex />
             </div>
             <div>
@@ -17,7 +18,8 @@ const NewsCenter = () => {
                 <div>
                     <Outlet />
                 </div>
-            </div>
+            </div> */}
+            <Error404 />
         </LayOut>
     )
 }
