79622332ca34c5ce590fe3764db67fad848ac687
EnAyTi <anhtuan222001@gmail.com>
2023-07-05 08:50:21 +0700

    style button tab

--- src/features/Market/utils/MarketTab.jsx
+++ src/features/Market/utils/MarketTab.jsx
@@ -1,58 +1,118 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import "./tabStyle.css";
+import "../../../utils/style/buttonStyle.css"
+
 const MarketTab = () => {
   return (
-    <div className="flex justify-around mb-3">
-      <NavLink
-        className={(params) => {
-          if (params.isActive) {
-            return "no-underline button dark:text-white text-black xs:text-[0.9rem] md:text-[1rem] lg:text-[1rem] xl:text-[1.2rem] 2xl:text-[1.2rem] font-bold w-[25%] text-center  relative";
-          } else {
-            return "no-underline button dark:text-white text-black xs:text-[0.9rem] md:text-[1rem] lg:text-[1rem] xl:text-[1.2rem] 2xl:text-[1.2rem] font-bold w-[25%] text-center opacity-70 ";
-          }
-        }}
-        to="/thi-truong/chi-so-thi-truong"
-      >
-        Chỉ số thị trường
-      </NavLink>
-      <NavLink
-        className={(params) => {
-          if (params.isActive) {
-            return "no-underline button dark:text-white text-black xs:text-[0.9rem] md:text-[1rem] lg:text-[1rem] xl:text-[1.2rem] 2xl:text-[1.2rem] font-bold w-[25%] text-center  relative";
-          } else {
-            return "no-underline button dark:text-white text-black xs:text-[0.9rem] md:text-[1rem] lg:text-[1rem] xl:text-[1.2rem] 2xl:text-[1.2rem] font-bold w-[25%] text-center opacity-70 ";
-          }
-        }}
-        to="/thi-truong/dong-tien-thi-truong"
-      >
-        Dòng tiền thị trường
-      </NavLink>
-      <NavLink
-        className={(params) => {
-          if (params.isActive) {
-            return "no-underline button dark:text-white text-black xs:text-[0.9rem] md:text-[1rem] lg:text-[1rem] xl:text-[1.2rem] 2xl:text-[1.2rem] font-bold w-[25%] text-center  relative";
-          } else {
-            return "no-underline button dark:text-white text-black xs:text-[0.9rem] md:text-[1rem] lg:text-[1rem] xl:text-[1.2rem] 2xl:text-[1.2rem] font-bold w-[25%] text-center opacity-70 ";
-          }
-        }}
-        to="/thi-truong/hieu-suat-va-dinh-gia"
-      >
-        Hiệu suất & định giá
-      </NavLink>
-      <NavLink
-        className={(params) => {
-          if (params.isActive) {
-            return "no-underline button dark:text-white text-black xs:text-[0.9rem] md:text-[1rem] lg:text-[1rem] xl:text-[1.2rem] 2xl:text-[1.2rem] font-bold w-[25%] text-center  relative";
-          } else {
-            return "no-underline button dark:text-white text-black xs:text-[0.9rem] md:text-[1rem] lg:text-[1rem] xl:text-[1.2rem] 2xl:text-[1.2rem] font-bold w-[25%] text-center opacity-70 ";
-          }
-        }}
-        to="/thi-truong/thi-truong-quoc-te"
-      >
-        Thị trường quốc tế
-      </NavLink>
-    </div>
+    <>
+      <div className="sm:block xs:hidden xxs:hidden">
+        <div className="flex justify-around mb-3">
+          <NavLink
+            className={(params) => {
+              if (params.isActive) {
+                return "no-underline buttonStyle text-white mx-1 font-bold w-[30%] text-center  relative";
+              } else {
+                return "no-underline buttonStyle text-white mx-1 font-bold w-[30%] text-center opacity-70 ";
+              }
+            }}
+            to="/thi-truong/chi-so-thi-truong"
+          >
+            Chỉ số thị trường
+          </NavLink>
+          <NavLink
+            className={(params) => {
+              if (params.isActive) {
+                return "no-underline buttonStyle text-white mx-1 font-bold w-[30%] text-center  relative";
+              } else {
+                return "no-underline buttonStyle text-white mx-1 font-bold w-[30%] text-center opacity-70 ";
+              }
+            }}
+            to="/thi-truong/dong-tien-thi-truong"
+          >
+            Dòng tiền thị trường
+          </NavLink>
+          <NavLink
+            className={(params) => {
+              if (params.isActive) {
+                return "no-underline buttonStyle text-white mx-1 font-bold w-[30%] text-center  relative";
+              } else {
+                return "no-underline buttonStyle text-white mx-1 font-bold w-[30%] text-center opacity-70 ";
+              }
+            }}
+            to="/thi-truong/hieu-suat-va-dinh-gia"
+          >
+            Hiệu suất & định giá
+          </NavLink>
+          <NavLink
+            className={(params) => {
+              if (params.isActive) {
+                return "no-underline buttonStyle text-white mx-1 font-bold w-[30%] text-center  relative";
+              } else {
+                return "no-underline buttonStyle text-white mx-1 font-bold w-[30%] text-center opacity-70 ";
+              }
+            }}
+            to="/thi-truong/thi-truong-quoc-te"
+          >
+            Thị trường quốc tế
+          </NavLink>
+        </div>
+      </div>
+
+      <div className="sm:hidden xs:block">
+        <div className="flex justify-around mb-3">
+          <NavLink
+            className={(params) => {
+              if (params.isActive) {
+                return "no-underline buttonStyle text-white mx-1 font-bold w-[30%] text-center  relative";
+              } else {
+                return "no-underline buttonStyle text-white mx-1 font-bold w-[30%] text-center opacity-70 ";
+              }
+            }}
+            to="/thi-truong/chi-so-thi-truong"
+          >
+            Chỉ số thị trường
+          </NavLink>
+          <NavLink
+            className={(params) => {
+              if (params.isActive) {
+                return "no-underline buttonStyle text-white mx-1 font-bold w-[30%] text-center  relative";
+              } else {
+                return "no-underline buttonStyle text-white mx-1 font-bold w-[30%] text-center opacity-70 ";
+              }
+            }}
+            to="/thi-truong/dong-tien-thi-truong"
+          >
+            Dòng tiền thị trường
+          </NavLink>
+        </div>
+        <div className="flex justify-around mb-2">
+          <NavLink
+            className={(params) => {
+              if (params.isActive) {
+                return "no-underline buttonStyle text-white mx-1 font-bold w-[30%] text-center  relative";
+              } else {
+                return "no-underline buttonStyle text-white mx-1 font-bold w-[30%] text-center opacity-70 ";
+              }
+            }}
+            to="/thi-truong/hieu-suat-va-dinh-gia"
+          >
+            Hiệu suất & định giá
+          </NavLink>
+          <NavLink
+            className={(params) => {
+              if (params.isActive) {
+                return "no-underline buttonStyle text-white mx-1 font-bold w-[30%] text-center  relative";
+              } else {
+                return "no-underline buttonStyle text-white mx-1 font-bold w-[30%] text-center opacity-70 ";
+              }
+            }}
+            to="/thi-truong/thi-truong-quoc-te"
+          >
+            Thị trường quốc tế
+          </NavLink>
+        </div>
+      </div>
+    </>
   );
 };
 
--- src/features/NewsCenter/NewsCenter.jsx
+++ src/features/NewsCenter/NewsCenter.jsx
@@ -1,22 +1,68 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom';
+import React, { useEffect, useState, Fragment } from 'react'
+import { Outlet, useLocation } from 'react-router-dom';
 import LayOut from '../../HOCs/Layout'
 import NewsCenterNavTab from './utils/component/NewsCenterNavTab'
 import InternationalIndex from '../Chart/components/InternationalIndex';
+import { useSelector } from 'react-redux';
+import Footer from "../../components/Footer";
+const apiUrl = process.env.REACT_APP_BASE_URL;
 
 const NewsCenter = () => {
+    const location = useLocation()
+    const [theme, setTheme] = useState(localStorage.getItem('theme'))
+    const color = useSelector((state) => state.color.colorTheme);
+
+    const [bannerDisplay, setBannerDisplay] = useState(false)
+    useEffect(() => {
+        if (location.pathname === "/trung-tam-tin-tuc") {
+            setBannerDisplay(true);
+        } else {
+            setBannerDisplay(false);
+        }
+    }, [location]);
+
+    useEffect(() => {
+        setTheme(color);
+    }, [color]);
+
     return (
         <LayOut>
             <div>
                 <InternationalIndex />
             </div>
-            <div>
+            <div className="container mx-auto">
                 <div>
-                    <NewsCenterNavTab />
+                    <div>
+                        <NewsCenterNavTab />
+                    </div>
+                    <div>
+                        <Outlet />
+                    </div>
                 </div>
                 <div>
-                    <Outlet />
+                    {bannerDisplay ? (
+                        <div className="h-auto pt-5 pb-2 flex justify-center ">
+                            <div className="flex  md:flex-row md:justify-around sm:flex-col sm:items-center xs:flex-col xs:items-center xxs:flex-col xxs:items-center w-[50%]">
+                                <div className="px-2 relative">
+                                    <a href="https://zalo.me/1623670409453822014" target="_blank" rel="noopener noreferrer">
+                                        <img className="xl:w-[670px] xl:h-[500px] lg:w-[447px] lg:h-[333px] md:w-[350px] md:h-[261px] sm:w-[350px] sm:h-[261px] xs:w-[350px] xs:h-[261px] xxs:w-[223px] xxs:h-[167px]" src={`${apiUrl}/resources/images/chat-bot-zalo.png`}
+                                            alt='zalo-banner' />
+                                    </a>
+
+                                </div>
+                                <div className="px-2 relative">
+                                    <a href="https://t.me/betaEmarketbot" target="_blank" rel="noopener noreferrer">
+                                        <img className="xl:w-[670px] xl:h-[500px] lg:w-[447px] lg:h-[333px] md:w-[350px] md:h-[261px] sm:w-[350px] sm:h-[261px] xs:w-[350px] xs:h-[261px] xxs:w-[223px] xxs:h-[167px]" src={`${apiUrl}/resources/images/chat-bot-tele.png`}
+                                            alt='tele-banner' />
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    ) : (
+                        <Fragment></Fragment>
+                    )}
                 </div>
+                <Footer />
             </div>
         </LayOut>
     )
