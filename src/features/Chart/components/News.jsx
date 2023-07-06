6e7b6885f941cddd5e89b0b7e6a813d24b63326c
EnAyTi <anhtuan222001@gmail.com>
2023-07-06 09:05:22 +0700

    fix marquee news

--- src/features/Chart/components/News.jsx
+++ src/features/Chart/components/News.jsx
@@ -3,26 +3,29 @@ import { useDispatch, useSelector } from "react-redux";
 import moment from "moment";
 import { fetchDataNews } from "../thunk";
 import '../utils/marquee.css'
+
 const News = () => {
   const dispatch = useDispatch()
   const dataNews = useSelector((state) => state.chart.dataNews);
+
   useEffect(() => {
     dispatch(fetchDataNews)
   }, [])
+
   return (
-    <div className=" dark:bg-black bg-white p-1 overflow-x-hidden">
-      <div className=" slide blog-news h-full  flex flex-nowrap ">
+    <div className=" dark:bg-black bg-white p-2 overflow-x-hidden">
+      <div className={`animate-[marquee_60s_linear_infinite] blog-news h-full flex flex-nowrap`}>
         {dataNews.data?.map((item, index) => {
           return (
-            <a key={index} className="card no-underline text-white" href={item.Href}>
-              <div className="card-body flex  w-[400px] h-[90px] m-2">
-                <div className="card-img  ">
+            <a key={index} className="card no-underline text-white" href={item.Href} target="_blank" rel="noopener noreferrer">
+              <div className="card-body flex w-[350px] h-[90px] m-1">
+                <div className="card-img">
                   <img src={item.Img} alt={item.Title} width={120} height={95} />
                 </div>
-                <div className="h-full flex flex-col  justify-around ml-2">
+                <div className="h-full flex flex-col justify-around ml-1">
                   <div className="card-text">
                     <p className="line-clamp-2 text-[0.75rem] font-semibold dark:text-white text-black items-center justify-center">  {item.Title}</p>
-                    <p className="line-clamp-2 text-[0.7rem] dark:text-white text-black items-center justify-center"> {item.SubTitle}</p>
+                    <p className="line-clamp-2 text-[0.7rem] dark:text-white text-black items-center justify-center mt-1.5"> {item.SubTitle}</p>
                     <span className="text-[0.65rem] dark:text-white text-black items-center justify-center"> {moment(item.Date).format("DD/MM/YYYY - HH:mm")}</span>
                   </div>
                 </div>
@@ -36,3 +39,4 @@ const News = () => {
 };
 
 export default News;
+
