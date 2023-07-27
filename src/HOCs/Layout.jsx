4d58158816ad4629fbe81ea413e988daeb7ff64c
EnAyTi <anhtuan222001@gmail.com>
2023-07-27 15:34:43 +0700

    build detailStock

--- src/HOCs/Layout.jsx
+++ src/HOCs/Layout.jsx
@@ -32,9 +32,9 @@ const LayOut = (props) => {
         {props.children}
       </section>
       {showScrollButton && (
-        <div className="btnBackToTop-container fixed bottom-[10%] right-[2%] z-30  xxs:right-[50%] xxs:bottom-[-4%] xxs:translate-x-[50%] xxs:hover:translate-y-[-20px] opacity-80 transition-all duration-500 hover:opacity-100 ">
+        <div className="btnBackToTop-container fixed bottom-[10%] right-[2%] z-30 xxs:right-[50%] xxs:bottom-[-4%] xxs:translate-x-[50%] xxs:hover:translate-y-[-20px] opacity-80 transition-all duration-500 hover:opacity-100 ">
           <button
-            className=" bg-orange-400  text-xl text-white rounded-full border-0  z-30 px-2 py-1  bg-transparent  "
+            className=" bg-orange-400 cursor-pointer text-xl text-white rounded-full border-0 z-30 px-2 py-1 bg-transparent  "
             onClick={handleScrollToTop} >
             <FaArrowUp />
           </button>
