873c0a83ce45188fe037297169cb340ab0dddc42
EnAyTi <anhtuan222001@gmail.com>
2023-07-31 13:13:43 +0700

    f5 theme change tab stockDetail

---
+++ src/features/Stock/components/TransactionStatistics/TotalMatchingVolume.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TotalMatchingVolume = () => {
+    return (
+        <div className='dark:text-white text-dark uppercase'>Total_Matching_Volume</div>
+    )
+}
+
+export default TotalMatchingVolume
---
+++ src/features/Stock/components/TransactionStatistics/TransactionData.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TransactionData = () => {
+    return (
+        <div className='dark:text-white text-dark uppercase'>Transaction_Data</div>
+    )
+}
+
+export default TransactionData
