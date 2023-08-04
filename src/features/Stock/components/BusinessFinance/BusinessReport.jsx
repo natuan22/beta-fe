1fdc05ae343f4699aff1788c39807a5e6b4d1428
EnAyTi <anhtuan222001@gmail.com>
2023-08-04 10:45:02 +0700

    fix button margin

--- src/features/Stock/components/BusinessFinance/BusinessReport.jsx
+++ src/features/Stock/components/BusinessFinance/BusinessReport.jsx
@@ -13,7 +13,7 @@ const BusinessReport = ({ codeUrl }) => {
             <div className='mt-4'>
                 <div className='md:flex sm:block justify-between'>
                     <div>
-                        <button className={`statementsCashFlows-btn ml-5  ${activeBtn === 0 ? 'statementsCashFlows-active-btn' : 'statementsCashFlows-btn-2'}`}
+                        <button className={`statementsCashFlows-btn ml-[48px]  ${activeBtn === 0 ? 'statementsCashFlows-active-btn' : 'statementsCashFlows-btn-2'}`}
                             onClick={() => {
                                 setActiveBtn(0)
                             }}>
