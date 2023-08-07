4ce8248e4ee2b2ae4c3248e4f81fbd25fe2697d3
EnAyTi <anhtuan222001@gmail.com>
2023-08-07 17:04:11 +0700

    swiper add

--- src/features/Stock/components/BusinessFinance/FinancialIndicators/utils/components/SlideComponent/SlideValuation.jsx
+++ src/features/Stock/components/BusinessFinance/FinancialIndicators/utils/components/SlideComponent/SlideValuation.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 
-const SlideValuation = () => {
+const SlideValuation = ({ data, labels }) => {
     return (
-        <div>SlideValuation</div>
+        <div>
+            <div>Chỉ số định giá</div>
+            <hr />
+            <div className='grid xl:grid-cols-2 lg:grid-cols-none gap-3'>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
     )
 }
 
