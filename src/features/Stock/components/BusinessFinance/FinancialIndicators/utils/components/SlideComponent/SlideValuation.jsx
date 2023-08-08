6498c5e5cb1cbf3439028c2b1a34dc3c3ff82a2b
baohuydev <lenambaohuy1998@gmail.com>
2023-08-08 09:51:16 +0700

    feat: financial indicator

--- src/features/Stock/components/BusinessFinance/FinancialIndicators/utils/components/SlideComponent/SlideValuation.jsx
+++ src/features/Stock/components/BusinessFinance/FinancialIndicators/utils/components/SlideComponent/SlideValuation.jsx
@@ -1,15 +1,27 @@
 import React from 'react'
+import ChartColumn from '../../../../components/ChartColumn'
 
-const SlideValuation = ({ data, labels }) => {
+const SlideValuation = ({ data, labels, time }) => {
+    const [PE, PB, EPS, BVPS] = data
+    console.log(time)
+    const [PELabel, PBLabel, EPSLabel, BVPSLabel] = labels
     return (
         <div>
             <div>Chỉ số định giá</div>
             <hr />
             <div className='grid xl:grid-cols-2 lg:grid-cols-none gap-3'>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
+                <div className='PE'>
+                    <ChartColumn data={PE} timeLine={time} name={PELabel} />
+                </div>
+                <div className='PB'>
+                    <ChartColumn data={PB} timeLine={time} name={PBLabel} />
+                </div>
+                <div className='EPS'>
+                    <ChartColumn data={EPS} timeLine={time} name={EPSLabel} />
+                </div>
+                <div className='BVPS'>
+                    <ChartColumn data={BVPS} timeLine={time} name={BVPSLabel} />
+                </div>
             </div>
         </div>
     )
