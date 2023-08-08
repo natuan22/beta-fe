39112ef0f32b21bc76e68da544f3450864e77b46
baohuydev <lenambaohuy1998@gmail.com>
2023-08-08 10:38:15 +0700

    feat: fincical indicator chart slide

--- src/features/Stock/components/BusinessFinance/FinancialIndicators/utils/components/SlideComponent/SlideEfficiency.jsx
+++ src/features/Stock/components/BusinessFinance/FinancialIndicators/utils/components/SlideComponent/SlideEfficiency.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import ChartColumn from '../../../../components/ChartColumn'
 
-const SlideEfficiency = ({ data, labels }) => {
-    console.log(labels)
+const SlideEfficiency = ({ data, labels, time }) => {
+    const [Arr1, Arr2, Arr3, Arr4] = data
+    const [Label1, Label2, Label3, Label4] = labels
+    console.log(data)
     return (
-        <div>SlideEfficiency</div>
+        <div>
+            <div>Hiệu quả hoạt động</div>
+            <hr />
+            <div className='grid xl:grid-cols-2 lg:grid-cols-none gap-3'>
+                <div className='wheelAsset'>
+                    <ChartColumn data={[Arr1]} timeLine={time} name={Label1} />
+                </div>
+                <div className='wheelTotalAsset'>
+                    <ChartColumn data={[Arr2]} timeLine={time} name={Label2} />
+                </div>
+                <div className='wheelCash'>
+                    <ChartColumn data={[Arr3]} timeLine={time} name={Label3} />
+                </div>
+                <div className='wheelVCSH'>
+                    <ChartColumn data={[Arr4]} timeLine={time} name={Label4} />
+                </div>
+            </div>
+        </div>
     )
 }
 
