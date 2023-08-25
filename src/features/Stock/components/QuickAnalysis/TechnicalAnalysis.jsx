9e82a892f9efad3a1eb5c1cec7a0c99c50f9ba09
baohuydev <lenambaohuy1998@gmail.com>
2023-08-25 11:23:22 +0700

    feat: rating

--- src/features/Stock/components/QuickAnalysis/TechnicalAnalysis.jsx
+++ src/features/Stock/components/QuickAnalysis/TechnicalAnalysis.jsx
@@ -1,10 +1,13 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchDataTechnicalAnalysis } from '../../thunk';
+import React, { useEffect, useState } from 'react'
 import { BsInfoCircleFill } from "react-icons/bs";
 import { Popover } from 'antd';
-
-const contentTechnicalAnalysis = (
+import ChartGauge from './components/ChartGauge';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchDataTechnicalAnalysis } from '../../thunk';
+import { Rating } from '@mui/material';
+import Loading from '../../../Chart/utils/Loading';
+import { getTextColorRating } from '../../../Chart/utils/utils';
+const contentFinancialHealthAnalysis = (
     <div>
         <span className='text-black font-medium rounded-lg text-sm bg-white p-2 '>
             Phân tích kỹ thuật
@@ -15,19 +18,120 @@ const contentTechnicalAnalysis = (
 const TechnicalAnalysis = ({ queryApi }) => {
     const dispatch = useDispatch()
     const { dataTechnicalAnalysis } = useSelector(state => state.stock)
-
+    const [showChild, setShowChild] = useState(false);
+    const [showChildStates, setShowChildStates] = useState([]);
     useEffect(() => {
         dispatch(fetchDataTechnicalAnalysis(queryApi.stock));
     }, [dispatch, queryApi]);
+    const toggleChildVisibility = (index) => {
+        const updatedStates = [...showChildStates];
+        updatedStates[index] = !updatedStates[index];
+        setShowChildStates(updatedStates);
+    };
     return (
         <div>
             <div className='border-solid dark:border-white border-b-[1px] border-t-0 border-x-0'>
                 <span className='dark:text-white text-black font-semibold w-[45%] flex justify-between'>Phân tích kỹ thuật
-                    <Popover content={contentTechnicalAnalysis} >
+                    <Popover content={contentFinancialHealthAnalysis} >
                         <span className='dark:text-white text-black'><BsInfoCircleFill /></span>
                     </Popover>
                 </span>
             </div>
+            {dataTechnicalAnalysis ? (
+                <div>
+                    <div className='grid grid-cols-2 items-center'>
+                        <ChartGauge data={dataTechnicalAnalysis.totalStar} />
+                        <div className='dark:text-white text-black text-justify'>Với những yếu tố đánh giá về Phân tích kỹ thuật cho thấy {queryApi.stock} đang có tính hiệu {getTextColorRating(dataTechnicalAnalysis.totalStar).text} với mức điểm số <span className={getTextColorRating(dataTechnicalAnalysis.totalStar).color}>{dataTechnicalAnalysis.totalStar}/5</span>.</div>
+                    </div>
+
+                    <div>
+                        <ul className='ml-[40px]'>
+                            {Array.isArray(dataTechnicalAnalysis.data) && dataTechnicalAnalysis.data?.map((item, index) => {
+                                const contentChild = (
+                                    <div>
+                                        <span className='text-black font-medium rounded-lg text-sm bg-white p-2'>
+                                            {item.name}
+                                        </span>
+                                    </div>
+                                );
+                                return (
+                                    <li key={index} className='dark:text-white text-black mb-2'>
+                                        <span className='items-center flex justify-between'>
+                                            <span className='w-[60%] flex justify-between'>
+                                                {item.name}
+                                                <Popover content={contentChild} onClick={() => toggleChildVisibility(index)}>
+                                                    <span className='dark:text-white text-black cursor-pointer'><BsInfoCircleFill /></span>
+                                                </Popover>
+                                            </span>
+                                            <span className='flex justify-between items-center text-base'>
+                                                <p className='mr-2'>{item.value}</p>
+                                                <Rating
+                                                    sx={{
+                                                        '& .MuiRating-iconEmpty': {
+                                                            color: '#faaf00'
+                                                        }
+                                                    }}
+                                                    value={item.value} readOnly />
+                                            </span>
+                                        </span>
+                                        {showChildStates[index] && (
+                                            <div>
+
+                                                <ul className='ml-[40px] mt-1'>
+                                                    {item.child.map((itemChild, index) => {
+                                                        const contentItemChild = (
+                                                            <div>
+                                                                <span className='text-black font-medium rounded-lg text-sm bg-white p-2'>
+                                                                    {itemChild.name}
+                                                                </span>
+                                                            </div>
+                                                        );
+                                                        return (
+                                                            <li key={index}>
+                                                                <span className='items-center flex justify-between'>
+
+                                                                    <span className='w-[56%] flex justify-between'>
+                                                                        <span className='text-sm'>
+                                                                            {itemChild.name}
+
+                                                                        </span>
+                                                                        <Popover content={contentItemChild} onClick={() => setShowChild(!showChild)}>
+                                                                            <span className='dark:text-white text-black cursor-pointer'><BsInfoCircleFill /></span>
+                                                                        </Popover>
+                                                                    </span>
+                                                                    <span className='flex justify-between items-center'>
+                                                                        <p className='text-[15px] mr-2'>
+                                                                            {itemChild.value}
+                                                                        </p>
+                                                                        <Rating
+                                                                            sx={{
+                                                                                '& .MuiRating-iconEmpty': {
+                                                                                    color: '#faaf00',
+                                                                                    fontSize: '20px'
+                                                                                },
+                                                                                '& .MuiRating-iconFilled ': {
+                                                                                    fontSize: '20px'
+
+                                                                                }
+                                                                            }}
+                                                                            value={itemChild.value} readOnly />
+                                                                    </span>
+                                                                </span>
+                                                            </li>
+                                                        )
+                                                    })}
+                                                </ul>
+                                            </div>
+                                        )}
+                                    </li>
+                                )
+                            })}
+                        </ul>
+                    </div>
+                </div>
+            ) : (
+                <div className='h-[300px] flex items-center justify-center'><Loading /></div>
+            )}
         </div>
     )
 }
