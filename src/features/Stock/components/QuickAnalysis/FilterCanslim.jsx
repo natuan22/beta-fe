bc48af4988a2feef3fe34f726df53e1ca6f92ad1
baohuydev <lenambaohuy1998@gmail.com>
2023-09-15 17:04:21 +0700

    feat:tool invest

--- src/features/Stock/components/QuickAnalysis/FilterCanslim.jsx
+++ src/features/Stock/components/QuickAnalysis/FilterCanslim.jsx
@@ -9,28 +9,13 @@ const FilterCanslim = ({ queryApi }) => {
     useEffect(() => {
         dispatch(fetchDataFilterCanslim(stock))
     }, [stock])
-
-
-
-
     return (
+
         <div className='text-white'>
-            <table class="table-auto">
-                <thead>
-                    <tr>
-                        <th>Tiêu chí</th>
-                        <th>Đánh giá</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {dataFilterCanslim?.map((item, index) => (
-                        <tr key={index}>
-                            <td className={`${item.name === 'Tổng hợp tiêu chí' ? 'font-bold' : ''} p-3`}>{item.name}</td>
-                            <td className={` p-3 text-center`}>{(item.value)}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
+            <div>
+
+            </div>
+
         </div>
     )
 }
