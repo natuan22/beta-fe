d20ae6325c411c1861d2322d7ca163ab1ed804c4
EnAyTi <anhtuan222001@gmail.com>
2023-09-18 10:19:46 +0700

    add ads to investTool

--- src/features/InvestTool/InvestTool.jsx
+++ src/features/InvestTool/InvestTool.jsx
@@ -1,25 +1,68 @@
-import React from 'react'
+import React, { Fragment, useEffect, useState } from 'react'
 import LayOut from '../../HOCs/Layout'
 import InternationalIndex from '../Chart/components/InternationalIndex'
 import News from '../Chart/components/News'
 import InvestToolTab from './utils/InvestToolTab'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
+import Banner from "../Chart/components/Banner";
+import Footer from "../../components/Footer";
+const apiUrl = process.env.REACT_APP_BASE_URL;
 
 const InvestTool = () => {
+    const location = useLocation()
+    const [bannerDisplay, setBannerDisplay] = useState(false)
+
+    useEffect(() => {
+        if (location.pathname === "/cong-cu-dau-tu") {
+            setBannerDisplay(true);
+        } else {
+            setBannerDisplay(false);
+        }
+    }, [location]);
+
     return (
-        <div>
-            <LayOut />
-            <InternationalIndex />
-            <News />
-
-            <div className='nav_bar'>
-                <InvestToolTab />
-            </div>
-
+        <LayOut>
             <div>
-                <Outlet />
+                <InternationalIndex />
+                <News />
             </div>
-        </div>
+            <Banner />
+            <div className='nav_bar container mx-auto'>
+                <div>
+                    <div className="px-11">
+                        <InvestToolTab />
+                    </div>
+
+                    <div>
+                        <Outlet />
+                    </div>
+                </div>
+                <div>
+                    {bannerDisplay ? (
+                        <div className="h-auto pt-5 pb-2 flex justify-center ">
+                            <div className="flex  md:flex-row md:justify-around sm:flex-col sm:items-center xs:flex-col xs:items-center xxs:flex-col xxs:items-center w-[50%]">
+                                <div className="px-2 relative">
+                                    <a href="https://zalo.me/1623670409453822014" target="_blank" rel="noopener noreferrer">
+                                        <img className="xl:w-[670px] xl:h-[500px] lg:w-[447px] lg:h-[333px] md:w-[350px] md:h-[261px] sm:w-[350px] sm:h-[261px] xs:w-[350px] xs:h-[261px] xxs:w-[223px] xxs:h-[167px]" src={`${apiUrl}/resources/images/chat-bot-zalo.png`}
+                                            alt='zalo-banner' />
+                                    </a>
+
+                                </div>
+                                <div className="px-2 relative">
+                                    <a href="https://t.me/betaEmarketbot" target="_blank" rel="noopener noreferrer">
+                                        <img className="xl:w-[670px] xl:h-[500px] lg:w-[447px] lg:h-[333px] md:w-[350px] md:h-[261px] sm:w-[350px] sm:h-[261px] xs:w-[350px] xs:h-[261px] xxs:w-[223px] xxs:h-[167px]" src={`${apiUrl}/resources/images/chat-bot-tele.png`}
+                                            alt='tele-banner' />
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    ) : (
+                        <Fragment></Fragment>
+                    )}
+                </div>
+                <Footer />
+            </div>
+        </LayOut>
     )
 }
 
