f38651045afdcd7853ed4b32093c2d378634ab7e
baohuydev <lenambaohuy1998@gmail.com>
2023-09-20 20:54:12 +0700

    fea: simulation

--- src/features/InvestTool/services/investToolService.js
+++ src/features/InvestTool/services/investToolService.js
@@ -6,5 +6,12 @@ export const investToolService = {
     },
     fetchDataStockFilter: (formData) => {
         return https.post('/api/v1/investment/filter', formData)
+    },
+    fetchStockList: (key_search) => {
+        return https.get('/api/v1/shares/search', {
+            params: {
+                key_search
+            }
+        })
     }
 }
--- src/features/InvestTool/utils/hashTb.js
+++ src/features/InvestTool/utils/hashTb.js
@@ -2,7 +2,7 @@ export const hashTbStockFilter = {
     "Thông tin doanh nghiệp": [
         { name: "Vốn hóa (tỷ VNĐ)", key: "marketCap" },
 
-        { name: "Thị giá (VNĐ)", key: "closePrice" },
+        { name: "Thị giá ( x1000 vnđ)", key: "closePrice" },
         { name: "KL cổ phiếu lưu hành (triệu CP)", key: "shareout" },
         ,
     ],
