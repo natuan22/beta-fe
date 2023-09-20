3cb8bd1f58c46b120a000b542f1b06c9feb5e751
baohuydev <lenambaohuy1998@gmail.com>
2023-09-20 13:18:25 +0700

    feat: invest tool - dispatch data to store

--- src/features/InvestTool/investToolSlice.js
+++ src/features/InvestTool/investToolSlice.js
@@ -3,7 +3,8 @@ import { investToolType } from "./utils/constant";
 
 
 const initialState = {
-    dataRangeMinMax: {}
+    dataRangeMinMax: {},
+    dataStockFilter: {}
 }
 
 
@@ -11,6 +12,9 @@ const reducer = (state = initialState, { type, payload }) => {
     return produce(state, (draft) => {
         if (type === investToolType.FETCH_DATA_RANGE_MIN_MAX) {
             draft.dataRangeMinMax = payload
+        }
+        if (type === investToolType.FETCH_DATA_STOCK_FILTER) {
+            draft.dataStockFilter = payload
         }
     })
 }
--- src/features/InvestTool/thunk.js
+++ src/features/InvestTool/thunk.js
@@ -18,7 +18,11 @@ export const fetchRangeMinMax = () => async dispatch => {
 export const fetchDataStockFilter = (formData) => async dispatch => {
     try {
         const res = await investToolService.fetchDataStockFilter(formData)
-        console.log(res)
+        console.log(res.data.data)
+        dispatch({
+            type: investToolType.FETCH_DATA_STOCK_FILTER,
+            payload: res.data.data
+        })
     } catch (err) {
         console.error(err)
     }
