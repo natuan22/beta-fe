e1bcf073ed99c2b2f19923a59f95fd834b301944
baohuydev <lenambaohuy1998@gmail.com>
2023-10-06 15:44:46 +0700

    change banner

--- src/features/Macro/Macro.jsx
+++ src/features/Macro/Macro.jsx
@@ -44,15 +44,14 @@ const Macro = () => {
                         <div className="h-auto pt-4 pb-2 flex justify-center ">
                             <div className="flex  md:flex-row md:justify-around sm:flex-col sm:items-center xs:flex-col xs:items-center xxs:flex-col xxs:items-center w-[50%]">
                                 <div className="px-2 relative">
-                                    <a href="https://zalo.me/1623670409453822014" target="_blank" rel="noopener noreferrer">
-                                        <img className="xl:w-[670px] xl:h-[500px] lg:w-[447px] lg:h-[333px] md:w-[350px] md:h-[261px] sm:w-[350px] sm:h-[261px] xs:w-[350px] xs:h-[261px] xxs:w-[223px] xxs:h-[167px]" src={`${apiUrl}/resources/images/chat-bot-zalo.png`}
+                                    <a href="https://trading.bsi.com.vn/login" target="_blank" rel="noopener noreferrer">
+                                        <img className="xl:w-[712px] xl:h-[500px] lg:w-[505px] lg:h-[333px] md:w-[370px] md:h-[261px] sm:w-[350px] sm:h-[261px] xs:w-[350px] xs:h-[261px] xxs:w-[223px] xxs:h-[167px]" src={`${apiUrl}/resources/images/banner1.png`}
                                             alt='zalo-banner' />
                                     </a>
-
                                 </div>
                                 <div className="px-2 relative">
-                                    <a href="https://t.me/betaEmarketbot" target="_blank" rel="noopener noreferrer">
-                                        <img className="xl:w-[670px] xl:h-[500px] lg:w-[447px] lg:h-[333px] md:w-[350px] md:h-[261px] sm:w-[350px] sm:h-[261px] xs:w-[350px] xs:h-[261px] xxs:w-[223px] xxs:h-[167px]" src={`${apiUrl}/resources/images/chat-bot-tele.png`}
+                                    <a href="https://www.bsi.com.vn/vn/tin-tuc/tin-va-su-kien/lai-margin-9-9-phi-giao-dich-0-1" target="_blank" rel="noopener noreferrer">
+                                        <img className="xl:w-[712px] xl:h-[500px] lg:w-[505px] lg:h-[333px] md:w-[370px] md:h-[261px] sm:w-[350px] sm:h-[261px] xs:w-[350px] xs:h-[261px] xxs:w-[223px] xxs:h-[167px]" src={`${apiUrl}/resources/images/banner2.png`}
                                             alt='tele-banner' />
                                     </a>
                                 </div>
