0471fac428e0ffb4a74a7e27daa45adb192cde7a
baohuydev <lenambaohuy1998@gmail.com>
2023-12-04 14:50:30 +0700

    docker

--- docker/build.sh
+++ docker/build.sh
@@ -2,5 +2,5 @@
 
 VERSION="0.0.1"
 
-sudo docker build -t chauminh1212/beta-fe:${VERSION} .
-sudo docker push chauminh1212/beta-fe:${VERSION}
+docker build -t baohuydev/beta-stock-frontend:${VERSION}
+docker push baohuydev/beta-stock-frontend:${VERSION}
