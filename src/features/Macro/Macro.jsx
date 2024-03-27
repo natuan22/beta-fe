76342ab3a4924740c9f5db4d8c5615b4a0db158e
Tuan Nguyen Anh <tuan.na@beta.com.vn>
2024-03-27 08:46:29 +0700

    fix to te ti

--- src/features/Macro/Macro.jsx
+++ src/features/Macro/Macro.jsx
@@ -1,70 +1,84 @@
-import React, { Fragment, useEffect, useState } from 'react'
-import LayOut from '../../HOCs/Layout'
-import MacroTabs from './utils/MacroTabs';
-import { Outlet, useLocation } from 'react-router-dom';
-import InternationalIndex from '../Chart/components/InternationalIndex';
-import News from '../Chart/components/News';
-import { useSelector } from 'react-redux';
+import React, { Fragment, useEffect, useState } from "react";
+import LayOut from "../../HOCs/Layout";
+import MacroTabs from "./utils/MacroTabs";
+import { Outlet, useLocation } from "react-router-dom";
+import InternationalIndex from "../Chart/components/InternationalIndex";
+import News from "../Chart/components/News";
+import { useSelector } from "react-redux";
 import Footer from "../../components/Footer";
-import Banner from '../Chart/components/Banner';
+import Banner from "../Chart/components/Banner";
 const apiUrl = process.env.REACT_APP_BASE_URL;
 
 const Macro = () => {
-    const location = useLocation()
-    const color = useSelector((state) => state.color.colorTheme);
+  const location = useLocation();
+  const color = useSelector((state) => state.color.colorTheme);
 
-    const [bannerDisplay, setBannerDisplay] = useState(false)
-    useEffect(() => {
-        if (location.pathname === "/vi-mo") {
-            setBannerDisplay(true);
-        } else {
-            setBannerDisplay(false);
-        }
-    }, [location]);
+  const [bannerDisplay, setBannerDisplay] = useState(false);
+  useEffect(() => {
+    if (location.pathname === "/vi-mo") {
+      setBannerDisplay(true);
+    } else {
+      setBannerDisplay(false);
+    }
+  }, [location]);
 
-    return (
-        <LayOut>
-            <div>
-                <InternationalIndex />
-                <News />
-            </div>
-            <Banner />
-            <div className="container mx-auto">
-                <div>
-                    <div className="px-11">
-                        <MacroTabs />
-                    </div>
+  return (
+    <LayOut>
+      <div>
+        <InternationalIndex />
+        <News />
+      </div>
+      <Banner />
+      <div className="container mx-auto">
+        <div>
+          <div className="px-11">
+            <MacroTabs />
+          </div>
 
-                    <div>
-                        <Outlet />
-                    </div>
+          <div>
+            <Outlet />
+          </div>
+        </div>
+        <div>
+          {bannerDisplay ? (
+            <div className="h-auto pt-4 pb-2 flex justify-center ">
+              <div className="flex md:flex-row md:justify-around sm:flex-col sm:items-center xs:flex-col xs:items-center xxs:flex-col xxs:items-center">
+                <div className="px-2 relative">
+                  <a
+                    href="https://zalo.me/1623670409453822014"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <img
+                      className="xl:w-[712px] xl:h-[500px] lg:w-[505px] lg:h-[333px] md:w-[370px] md:h-[261px] sm:w-[350px] sm:h-[261px] xs:w-[350px] xs:h-[261px] xxs:w-[223px] xxs:h-[167px]"
+                      src={`${apiUrl}/resources/images/banner1.png`}
+                      alt="zalo-banner"
+                    />
+                  </a>
                 </div>
-                <div>
-                    {bannerDisplay ? (
-                        <div className="h-auto pt-4 pb-2 flex justify-center ">
-                            <div className="flex  md:flex-row md:justify-around sm:flex-col sm:items-center xs:flex-col xs:items-center xxs:flex-col xxs:items-center w-[50%]">
-                                <div className="px-2 relative">
-                                    <a href="https://zalo.me/1623670409453822014" target="_blank" rel="noopener noreferrer">
-                                        <img className="xl:w-[712px] xl:h-[500px] lg:w-[505px] lg:h-[333px] md:w-[370px] md:h-[261px] sm:w-[350px] sm:h-[261px] xs:w-[350px] xs:h-[261px] xxs:w-[223px] xxs:h-[167px]" src={`${apiUrl}/resources/images/banner1.png`}
-                                            alt='zalo-banner' />
-                                    </a>
-                                </div>
-                                <div className="px-2 relative">
-                                    <a href="https://t.me/betaEmarketbot" target="_blank" rel="noopener noreferrer">
-                                        <img className="xl:w-[712px] xl:h-[500px] lg:w-[505px] lg:h-[333px] md:w-[370px] md:h-[261px] sm:w-[350px] sm:h-[261px] xs:w-[350px] xs:h-[261px] xxs:w-[223px] xxs:h-[167px]" src={`${apiUrl}/resources/images/banner2.png`}
-                                            alt='tele-banner' />
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    ) : (
-                        <Fragment></Fragment>
-                    )}
+                <div className="px-2 relative">
+                  <a
+                    href="https://t.me/betaEmarketbot"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <img
+                      className="xl:w-[712px] xl:h-[500px] lg:w-[505px] lg:h-[333px] md:w-[370px] md:h-[261px] sm:w-[350px] sm:h-[261px] xs:w-[350px] xs:h-[261px] xxs:w-[223px] xxs:h-[167px]"
+                      src={`${apiUrl}/resources/images/banner2.png`}
+                      alt="tele-banner"
+                    />
+                  </a>
                 </div>
-                <Footer />
+              </div>
             </div>
-        </LayOut>
-    )
-}
+          ) : (
+            <Fragment></Fragment>
+          )}
+        </div>
+        <Footer />
+      </div>
+    </LayOut>
+  );
+};
 
-export default Macro
+export default Macro;
--- src/features/NewsCenter/NewsCenter.jsx
+++ src/features/NewsCenter/NewsCenter.jsx
@@ -1,64 +1,77 @@
-import React, { useEffect, useState, Fragment } from 'react'
-import { Outlet, useLocation } from 'react-router-dom';
-import LayOut from '../../HOCs/Layout'
-import NewsCenterNavTab from './utils/component/NewsCenterNavTab'
-import InternationalIndex from '../Chart/components/InternationalIndex';
+import React, { useEffect, useState, Fragment } from "react";
+import { Outlet, useLocation } from "react-router-dom";
+import LayOut from "../../HOCs/Layout";
+import NewsCenterNavTab from "./utils/component/NewsCenterNavTab";
+import InternationalIndex from "../Chart/components/InternationalIndex";
 import Footer from "../../components/Footer";
 const apiUrl = process.env.REACT_APP_BASE_URL;
 
 const NewsCenter = () => {
-    const location = useLocation()
-    const [bannerDisplay, setBannerDisplay] = useState(false)
+  const location = useLocation();
+  const [bannerDisplay, setBannerDisplay] = useState(false);
 
-    useEffect(() => {
-        if (location.pathname === "/trung-tam-tin-tuc") {
-            setBannerDisplay(true);
-        } else {
-            setBannerDisplay(false);
-        }
-    }, [location]);
+  useEffect(() => {
+    if (location.pathname === "/trung-tam-tin-tuc") {
+      setBannerDisplay(true);
+    } else {
+      setBannerDisplay(false);
+    }
+  }, [location]);
 
-    return (
-        <LayOut>
-            <div>
-                <InternationalIndex />
-            </div>
-            <div className="container mx-auto">
-                <div>
-                    <div>
-                        <NewsCenterNavTab />
-                    </div>
-                    <div>
-                        <Outlet />
-                    </div>
+  return (
+    <LayOut>
+      <div>
+        <InternationalIndex />
+      </div>
+      <div className="container mx-auto">
+        <div>
+          <div>
+            <NewsCenterNavTab />
+          </div>
+          <div>
+            <Outlet />
+          </div>
+        </div>
+        <div>
+          {bannerDisplay ? (
+            <div className="h-auto pt-5 pb-2 flex justify-center ">
+              <div className="flex md:flex-row md:justify-around sm:flex-col sm:items-center xs:flex-col xs:items-center xxs:flex-col xxs:items-center">
+                <div className="px-2 relative">
+                  <a
+                    href="https://zalo.me/1623670409453822014"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <img
+                      className="xl:w-[712px] xl:h-[500px] lg:w-[505px] lg:h-[333px] md:w-[370px] md:h-[261px] sm:w-[350px] sm:h-[261px] xs:w-[350px] xs:h-[261px] xxs:w-[223px] xxs:h-[167px]"
+                      src={`${apiUrl}/resources/images/banner1.png`}
+                      alt="zalo-banner"
+                    />
+                  </a>
                 </div>
-                <div>
-                    {bannerDisplay ? (
-                        <div className="h-auto pt-5 pb-2 flex justify-center ">
-                            <div className="flex  md:flex-row md:justify-around sm:flex-col sm:items-center xs:flex-col xs:items-center xxs:flex-col xxs:items-center w-[50%]">
-                                <div className="px-2 relative">
-                                    <a href="https://zalo.me/1623670409453822014" target="_blank" rel="noopener noreferrer">
-                                        <img className="xl:w-[712px] xl:h-[500px] lg:w-[505px] lg:h-[333px] md:w-[370px] md:h-[261px] sm:w-[350px] sm:h-[261px] xs:w-[350px] xs:h-[261px] xxs:w-[223px] xxs:h-[167px]" src={`${apiUrl}/resources/images/banner1.png`}
-                                            alt='zalo-banner' />
-                                    </a>
-
-                                </div>
-                                <div className="px-2 relative">
-                                    <a href="https://t.me/betaEmarketbot" target="_blank" rel="noopener noreferrer">
-                                        <img className="xl:w-[712px] xl:h-[500px] lg:w-[505px] lg:h-[333px] md:w-[370px] md:h-[261px] sm:w-[350px] sm:h-[261px] xs:w-[350px] xs:h-[261px] xxs:w-[223px] xxs:h-[167px]" src={`${apiUrl}/resources/images/banner2.png`}
-                                            alt='tele-banner' />
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    ) : (
-                        <Fragment></Fragment>
-                    )}
+                <div className="px-2 relative">
+                  <a
+                    href="https://t.me/betaEmarketbot"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <img
+                      className="xl:w-[712px] xl:h-[500px] lg:w-[505px] lg:h-[333px] md:w-[370px] md:h-[261px] sm:w-[350px] sm:h-[261px] xs:w-[350px] xs:h-[261px] xxs:w-[223px] xxs:h-[167px]"
+                      src={`${apiUrl}/resources/images/banner2.png`}
+                      alt="tele-banner"
+                    />
+                  </a>
                 </div>
-                <Footer />
+              </div>
             </div>
-        </LayOut>
-    )
-}
+          ) : (
+            <Fragment></Fragment>
+          )}
+        </div>
+        <Footer />
+      </div>
+    </LayOut>
+  );
+};
 
-export default NewsCenter
+export default NewsCenter;
